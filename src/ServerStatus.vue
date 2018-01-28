ef096738195309d527efae3dc98ceba051b9560d
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-01-28 16:48:39 -0600

    Added components

---
+++ src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Server Status: {{status}}</h1>
+        <hr>
+        <button @click="changeStatus">Change</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data: function(){
+        return {
+            status: 'OK'
+        }
+    },
+    methods: {
+        changeStatus: function(){
+            this.status = "Critical!"
+        }
+    }
+}
+</script>
